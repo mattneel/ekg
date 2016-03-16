774624f28a051cc64aef97318c4e8e30e4d919a0
Matthew Neel <matt@mrneel.com>
2016-03-16 17:50:09 +0000

    Added prod.secret.exs

---
+++ config/prod.secret.exs
@@ -0,0 +1,13 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :ekg, Ekg.Endpoint,
+  secret_key_base: "kHj6kH2BtdiyVWfS76FyOTmn5IZHoLulPK7JVq7hwhIGdwgUf67eTiRCSBphFN0r"
+
+# Configure your database
+config :ekg, Ekg.Repo,
+  adapter: Sqlite.Ecto,
+  database: "db/ekg_prod.sqlite",
+  pool_size: 20
--- mkrel.sh
+++ mkrel.sh
@@ -1,1 +1,1 @@
-brunch build --production && MIX_ENV=prod mix phoenix.digest && MIX_ENV=prod mix compile && MIX_ENV=prod mix release
+MIX_ENV=prod mix phoenix.digest && MIX_ENV=prod mix compile && MIX_ENV=prod mix release
